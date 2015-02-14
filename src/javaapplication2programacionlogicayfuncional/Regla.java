66150a93c1064d0ea0d02aea4e2a098fb486d878
10031066 <edgar_5_11@hotmail.com>
2015-02-13 22:21:56 -0600

    Regla

--- src/javaapplication2programacionlogicayfuncional/Regla.java
+++ src/javaapplication2programacionlogicayfuncional/Regla.java
@@ -4,7 +4,20 @@ package javaapplication2programacionlogicayfuncional;
  * @author Edgar
  */
 public class Regla {
-    public Regla(){
+    int id;
+    String cosecuente;
+    Regla antecedente[][];
+    public Regla(String cosecuente,int i){
+        this.id = i;
+        this.cosecuente = cosecuente;
+    }
+    public void AgregaAntecedente(Regla[] reglas){//solo agrega 1 linea, es solamente para conjunciones
+        this.antecedente[0]=reglas;
+
+    }
+    private void Conjuncion(){
+        
         
     }
+    
 }
